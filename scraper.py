fce2ae9551fb25e8f267f9d4ff247f6a63a86cdd
haywoodjustin <49731705+haywoodjustin@users.noreply.github.com>
2023-04-05 16:39:45 -0400

    Correctly finding lines of dialog

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ import csv
 trans = [
     ["Entry", "Season", "Episode_Num", "Episode_Title", "Speaker", "Line"],
 ]
-entry = 1; 
 
 episode_doc = req.get('https://rickandmorty.fandom.com/wiki/Pilot/Transcript')
 
@@ -13,11 +12,10 @@ if(episode_doc):
     S = BeautifulSoup(episode_doc.content , 'html.parser')
     # print(S.prettify())
 
-    listOfBold =  S.find_all('div', class_='poem')
-    print(listOfBold[0]) 
-    # print(listOfBold)
-    # for line in listOfBold:
-    #     print(line)
+    lines =  S.find('div', {"class": "poem"})
+    lines = lines.findChildren()
+    
+    for line in lines:
+        print(line)
 
-    # print(trans) 
         
