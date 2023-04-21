25e579031585bad90ca57e2508078521fc54c7ad
Justin Nordeman <nordemja@mail.uc.edu>
2023-04-21 13:29:08 -0400

    filtering out stop/filler words for word cloud

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function get_char_stats(data, char_name)
             total_seasons: total_seasons,
             lines_by_ep: lines_by_ep
         }
-        console.log(char_stats)
+        //console.log(char_stats)
         return char_stats;
         
 }
@@ -66,10 +66,18 @@ function get_linechart_data(stats)
 
         line_chart_data.push(temp);
     }
-console.log(line_chart_data);
 
 return line_chart_data;
 }
+
+function getStopWords() {
+    stop_words = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"]
+    for (let i = 0; i < stop_words.length; i++) {
+        stop_words[i] = stop_words[i].toUpperCase()
+    }
+    return stop_words
+}
+
 
 let data, linechart; 
 let all_character_stats; 
@@ -97,16 +105,22 @@ d3.csv("/data/data.csv")
         ],
     });
 
+    stop_words = getStopWords()
+
     _data.forEach(d => {
         temp_line = d.line.replace(/[.,;!?":'-]/g, "")
         line_list = temp_line.split(" ")
         line_list.forEach(l => {
             l = l.toUpperCase()
+
+            if (!stop_words.includes(l) && l != "") {
+
             if (wordDict[l] == undefined) {
                 wordDict[l] = 1
             } else {
                 wordDict[l] += 1
             }
+        }
         })
     })
 
@@ -128,7 +142,7 @@ d3.csv("/data/data.csv")
         parentElement: '#word_cloud',
         containerHeight: 300,
         containerWidth: 466.83
-    }, _data, "day", "Day of Week", "Total Calls", "Amount of Calls Per Day");
+    }, _data, wordArr, "Day of Week", "Total Calls", "Amount of Calls Per Day");
 
     all_character_stats = 
     {
